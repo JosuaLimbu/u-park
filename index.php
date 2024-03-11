67534721f30b176a887b4e1eddc57031a06aa3ad
JosuaLimbu <osaltusal@gmail.com>
2024-03-11 17:19:49 +0800

    fix bug role

--- index.php
+++ index.php
@@ -15,15 +15,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['password'];
     $role = $_POST['role'];
 
-    $sql = "SELECT * FROM tbl_account WHERE username='$username' AND password='$password'";
+    $sql = "SELECT * FROM tbl_account WHERE role='$role' AND username='$username' AND password='$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $_SESSION['username'] = $username;
         $_SESSION['role'] = $role;
-        if ($role == 'admin') {
+        if ($role == 'Admin') {
             header("Location: $role/home/home.php");
-        } elseif ($role == 'operator') {
+        } elseif ($role == 'Operator') {
             header("Location: $role/home/home.php");
         }
     } else {
