ce89964070325e102318a346c368f566d9c8210a
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-03-25 15:44:21 +0800

    platedetection

--- operator/platedetection/platedetection.php
+++ operator/platedetection/platedetection.php
@@ -4,9 +4,7 @@ if (!isset ($_SESSION["username"]) || !isset ($_SESSION["role"]) || $_SESSION["r
     header("Location: http://localhost/upark");
 }
 $page = 'platedetection'; //buat page aktif di sidebar
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +14,7 @@ $page = 'platedetection'; //buat page aktif di sidebar
     <title>Plate Detection</title>
     <link rel="icon" type="image/png" href="../../img/U-Park.png">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="platedetection.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 </head>
 <style>
@@ -43,7 +41,31 @@ $page = 'platedetection'; //buat page aktif di sidebar
                     <p>Plate Detection</p>
                 </div>
             </div>
-
+            <div class="gate-container">
+                <div class="gate">
+                    <h5>Entrance Gate</h5>
+                    <button class="btn btn-info" id="enableButton"><i class='bx bx-camera'></i> Enable</button>
+                    <button class="btn btn-info" id="disableButton"><i class='bx bx-camera-off'></i> Disable</button>
+                </div>
+            </div>
+            <br>
+            <div class="gate-content-container" style="display: none;">
+                <div class=" gate-content-left">
+                    <div>
+                        <p>Entrance Gate</p>
+                        <img id="detectedImage" src="../../yolov5/videostream/0_detected.jpg" alt="Image Description">
+                    </div>
+                </div>
+                <div class="gate-content-right">
+                    <div>
+                        <label class="switch">
+                            <input type="checkbox" id="gateSwitch">
+                            <span class="slider round"></span>
+                        </label>
+                        <p id="gateStatus">Close Gate</p>
+                    </div>
+                </div>
+            </div>
 
         </main>
         <!-- MAIN -->
@@ -53,6 +75,34 @@ $page = 'platedetection'; //buat page aktif di sidebar
     <script src="../components/js/script.js"></script>
     <script src="../components/js/datetime.js"></script>
     <script src="../components/js/dropdown.js"></script>
+    <script>
+        function updateImage() {
+            var detectedImage = document.getElementById("detectedImage");
+            detectedImage.src = "../../yolov5/videostream/0_detected.jpg?timestamp=" + new Date().getTime();
+        }
+        setInterval(updateImage, 100);
+
+        document.getElementById("enableButton").addEventListener("click", function () {
+            document.querySelector(".gate-content-container").style.display = "flex";
+            fetch("http://localhost:5000/opencam");
+        });
+
+        document.getElementById("disableButton").addEventListener("click", function () {
+            document.querySelector(".gate-content-container").style.display = "none";
+            fetch("http://localhost:5000/closecam");
+        });
+
+
+        document.getElementById("gateSwitch").addEventListener("change", function () {
+            var gateStatusText = document.getElementById("gateStatus");
+            if (this.checked) {
+                gateStatusText.textContent = "Open Gate";
+            } else {
+                gateStatusText.textContent = "Close Gate";
+            }
+        });
+    </script>
+
 </body>
 
 </html>
